beaf3a24b860ffcef8cbb5aa2957201188d8e6e5
yuecheng11 <yuecheng011@sina.cn>
2017-04-19 18:51:28 +0800

    ee

---
+++ example_1/mysqlmanager.h
@@ -0,0 +1,18 @@
+#ifndef _MYSQLMANAGER_H_
+#define _MYSQLMANAGER_H_
+
+#include <mysql.h>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class MysqlManager
+{
+public:
+	
+private:
+	
+};
+
+#endif
